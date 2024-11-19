3cab0aefd8d0477b69b9066558270ef968fe7794
Zach Kusz <z.s.kusz@gmail.com>
2024-11-19 14:19:59 -0600

    add hover shadow animation

--- src/lib/components/WeatherCard.svelte
+++ src/lib/components/WeatherCard.svelte
@@ -45,12 +45,29 @@
 
 <style>
     .card {
+        position: relative;
         text-align: center;
         border: 4px solid indigo;
         border-radius: var(--bd-radius);
         padding: 2rem;
         box-shadow: 0px 0px 20px -8px rgb(238, 13, 140);
     }
+    .card::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        box-shadow: 0 0px 15px 10px rgb(118, 11, 194);
+        transition: opacity 400ms ease-in-out;
+    }
+    .card:hover::after {
+        opacity: 1;
+    }
+
     pre {
         text-wrap: auto;
     }
