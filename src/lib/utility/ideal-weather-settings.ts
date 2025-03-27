2413c6f35804e6196558c42ca9fd63f5ceb7ed47
Zach Kusz <z.s.kusz@gmail.com>
2025-03-27 18:25:03 -0500

    increase all wind speed tolerances

--- src/lib/utility/ideal-weather-settings.ts
+++ src/lib/utility/ideal-weather-settings.ts
@@ -1,6 +1,6 @@
-export const worstAlllowedWindSpeedMax = 12;
-export const okWindSpeedMax = 8;
-export const bestWindSpeedMax = 4;
-export const worstAllowedGustsMax = 16;
-export const okGustsMax = 8;
-export const bestGustsMax = 5;
+export const worstAlllowedWindSpeedMax = 13;
+export const okWindSpeedMax = 9;
+export const bestWindSpeedMax = 5;
+export const worstAllowedGustsMax = 19;
+export const okGustsMax = 15;
+export const bestGustsMax = 9;
