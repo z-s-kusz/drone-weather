a92e677b6e007f9fc28258d1b16963b33d5b41fc
z-s-kusz <z.s.kusz@gmail.com>
2025-05-21 19:22:43 -0500

    narrow 'good' weather criteria

--- src/lib/utility/ideal-weather-settings.ts
+++ src/lib/utility/ideal-weather-settings.ts
@@ -1,6 +1,6 @@
-export const worstAlllowedWindSpeedMax = 13;
-export const okWindSpeedMax = 9;
-export const bestWindSpeedMax = 5;
-export const worstAllowedGustsMax = 19;
-export const okGustsMax = 15;
-export const bestGustsMax = 9;
+export const worstAlllowedWindSpeedMax = 12;
+export const okWindSpeedMax = 8;
+export const bestWindSpeedMax = 4;
+export const worstAllowedGustsMax = 18;
+export const okGustsMax = 13;
+export const bestGustsMax = 6;
